82097ebe3931aa9347c84dcf9852c6c3a3da1d36
slavova-ralitsa <ralislavova8@abv.bg>
2026-05-27 17:54:01 +0300

    Restructure project directories

--- src/main/java/com/example/tripplanner/security/config/SecurityConfig.java
+++ src/main/java/com/example/tripplanner/security/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.example.tripplanner.security.jwt.JwtAuthEntryPoint;
 import com.example.tripplanner.security.jwt.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -23,6 +24,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
         http.csrf(AbstractHttpConfigurer::disable);
         http.exceptionHandling(exception -> exception.authenticationEntryPoint(authEntryPoint));
         http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
